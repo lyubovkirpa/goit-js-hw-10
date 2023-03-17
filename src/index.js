85d503259c8bd635255d1dde160182302658ab5b
Lyubov Kirpa <kirpa.lyubov.a@gmail.com>
2023-03-18 01:37:15 +0200

    add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import { debounse } from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputCountry = document.querySelector('input');
+const listCountry = document.querySelector('.country-list');
+const infoCountry = document.querySelector('.country-info');
+
+inputCountry.addEventListener(
+  'input',
+  debounse(onInputCountry, DEBOUNCE_DELAY)
+);
+
+function onInputCountry(e) {
+  const countryName = e.target.value.trim();
+  if (countryName !== '') {
+    deleteSymbol();
+  }
+}
+
+function deleteSymbol() {
+  listCountry.innerHTML = '';
+  infoCountry.innerHTML = '';
+}
